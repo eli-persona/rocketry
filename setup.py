2e632583c22f89af9a9a93e38f310819ddad7773
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-12-15 18:50:13 +1100

    pin to redbird fork

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 import sys
 
-sys.stderr.write("""
+sys.stderr.write(
+    """
 Unsupported installation method: python setup.py
 Please use `python -m pip install .` instead.
 """
 )
-#sys.exit(1)
+# sys.exit(1)
 from setuptools import setup
 
 setup(
     name="rocketry",
-    install_requires = [
-        'python-dateutil', 'redbird>=0.5.0', 'pydantic'
+    install_requires=[
+        "python-dateutil",
+        "redbird @ git+https://github.com/tekumara/red-bird.git@5e884da61abf1ef01d9a8ef0731ed89ea65964ab",
+        "pydantic",
     ],
 )
